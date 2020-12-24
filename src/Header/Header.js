5bc54c629179bd4070920fabd6473f30ae96b8c9
naim-007 <naim1823007@gmail.stud.kuet.ac.bd>
2020-12-25 00:21:30 +0600

    done

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,12 +6,12 @@ AOS.init({
 });
 const Header = () => {
     return (
-        <div className='container py-5' style={{ padding: '' }}>
+        <div className='container' style={{ padding: '' }}>
             <div className='d-flex flex-wrap justify-content-center align-items-center'>
-                <div className='col-md-6 my-5' data-aos='' >
+                <div className='col-md-6 my-3' data-aos='' >
                     <iframe style={{ width: '100%', height: '250px' }} src="https://www.youtube.com/embed/kFthJgdiHrw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
-                <div className='col-md-6 text-left' >
+                <div className='col-md-6 text-left my-3' >
                     <p className='pl-3' style={{ fontWeight: 'bold', fontFamily: "Comic Neue, cursive", fontSize: '20px', borderLeft: '4px solid rgba(243, 52, 89, 1)', borderRadius: '2px' }}>Sudah coba rutin upload video, share link ke segala penjuru, mendaki gunung, lewatin lembah tapi viewers dan subscribermu mogok segitu aja? <br /><br />
                     eiiits, jangan pesimis dulu bosque. Inilah saatnya PENDEKAR TURUN GUNUNG!<br /><br />
                     siapa tau, kamilah yang ditakdirkan jadi jawaban terakhir semua gundahmu <br /> #UhukLebaay</p>
--- src/Subscriber/Subscriber.js
+++ src/Subscriber/Subscriber.js
@@ -5,9 +5,8 @@ const Subscriber = () => {
         <div className='container mb-5' style={{backgroundColor: '', borderRadius:'20px', textAlign:'center', padding:'65px 0px 10px',fontFamily: "Comic Neue, cursive", fontSize:'20px'}}>
             <h2>KITA BAGI TUGAS SOB</h2>
             <p>"Tugasmu meningkatkan kualitas konten. Sisanya serahkan pada kami"</p>
-            <div style={{backgroundColor: 'rgba(243, 52, 89, 1)', borderTop: '1px solid white', borderBottom: '1px solid white', borderRadius:'10px', color: 'white', fontSize:'25px', lineHeight: '15px', padding: '20px'}}>
-                <p>Mulai Sekarang</p>
-                <p>Kita Se-SOHIB Seperjuangan!</p>
+            <div className='col-6 col-md-3 m-auto' style={{backgroundColor: 'rgba(243, 52, 89, 1)', borderTop: '1px solid white', borderBottom: '1px solid white', borderRadius:'10px', color: 'white', fontSize:'16px', padding: ''}}>
+                <p className='text-center mt-2'>Mulai Sekarang <br/> Kita Se-SOHIB Seperjuangan!</p>
             </div>
         </div>
     );
