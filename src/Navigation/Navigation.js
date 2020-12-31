013b5909ca1ba8a0bc16eb18eacdc70ca15620af
naim-007 <naim1823007@gmail.stud.kuet.ac.bd>
2020-12-31 08:34:58 +0600

    done

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -2,29 +2,31 @@ import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import { NavLink, useHistory } from 'react-router-dom';
 import logo from '../logo1.png'
+import caption from '../caption.png'
 const Navigation = () => {
     const history = useHistory()
 
     return (
         <div className="" id='header'>
-            <Navbar expand="lg" className='fixed-top px-5' style={{ backgroundColor: 'rgba(230, 0, 35, 1)' }}>
+            <Navbar expand="lg" className='fixed-top px-5 shadow' style={{ backgroundColor: 'white', }}>
                 <NavLink to="/">
+                <img style={{ width: '50px', height:'50px'}}src={caption} alt=""/>
                     <img style={{ width: '200px', height:'50px'}}src={logo} alt=""/>
                 </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" id='nav-bar' />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto mb-3">
-                        <NavLink className="text-decoration-none font-weight-bold text-white mx-4 mt-3 text-center" to="/">HOME</NavLink>
-                        <a className="text-decoration-none font-weight-bold text-white mx-4 mt-3 text-center" href="#features">FITUR</a>
-                        <a className="text-decoration-none font-weight-bold text-white mx-4 mt-3 text-center" href='#review'>REVIEW</a>
-                        <a className="text-decoration-none font-weight-bold text-white mx-4 mt-3 text-center" href="#faq">FAQ</a>
-                        <a className="text-decoration-none font-weight-bold text-white mx-4 mt-3 text-center" href="#blog">BLOG</a>
+                        <NavLink className="text-decoration-none font-weight-bold text-dark mx-4 mt-3 text-center" to="/">HOME</NavLink>
+                        <a className="text-decoration-none font-weight-bold text-dark mx-4 mt-3 text-center" href="#features">FITUR</a>
+                        <a className="text-decoration-none font-weight-bold text-dark mx-4 mt-3 text-center" href='#review'>REVIEW</a>
+                        <a className="text-decoration-none font-weight-bold text-dark mx-4 mt-3 text-center" href="#faq">FAQ</a>
+                        <a className="text-decoration-none font-weight-bold text-dark mx-4 mt-3 text-center" href="#blog">BLOG</a>
                     </Nav>
                     <div className="text-center mx-3 my-2">
-                        <button className='btn bg-white text-dark px-5' onClick={() => history.push('/login')} style={{ borderRadius: '20px', fontWeight: 'bold', fontSize: '17px' }} >Login</button>
+                        <button className='btn text-white px-5' onClick={() => history.push('/login')} style={{ borderRadius: '20px', fontWeight: 'bold', fontSize: '17px', backgroundColor: 'rgba(230, 0, 35, 1)'}} >Login</button>
                     </div>
                     <div className="text-center mx-3 my-2">
-                        <button className='btn bg-white text-dark  px-5' onClick={() => history.push('/signup')} style={{ borderRadius: '20px', fontWeight: 'bold', fontSize: '17px' }} >Signup</button>
+                        <button className='btn text-white  px-5' onClick={() => history.push('/signup')} style={{ borderRadius: '20px', fontWeight: 'bold', fontSize: '17px', backgroundColor: 'rgba(230, 0, 35, 1)' }} >Signup</button>
                     </div>
                 </Navbar.Collapse>
             </Navbar>
